eafe4747273969c1896ad00818dbbfceeec00902
mateeusferro <mateus.03fernandesaraujo@gmail.com>
2024-07-10 21:36:09 -0300

    init: first commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LoanModule } from './loan/loan.module';
+import { LoanModule } from './loan/loan.module';
+
+@Module({
+  imports: [LoanModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
